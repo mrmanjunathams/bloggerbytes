f08ba494167be7c9788f15fd57ed1e23a126dc5e
Manjunatha M S <manjunatha.ms@sakhatech.com>
2020-07-26 15:35:32 +0530

    ...

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private breakpointObserver: BreakpointObserver, private router: Router) { }
+
+  ngOnInit() {
+
+  }
+}
